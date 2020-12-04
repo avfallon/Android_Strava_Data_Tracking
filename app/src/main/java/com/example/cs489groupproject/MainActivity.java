0a73d747f259a981dfeda52f6b5833ff3f5d3de9
avfallon <31700889+avfallon@users.noreply.github.com>
2020-12-04 12:32:33 -0500

    THis works on Atom but not Android Studio

--- app/src/main/java/com/example/cs489groupproject/MainActivity.java
+++ app/src/main/java/com/example/cs489groupproject/MainActivity.java
@@ -3,6 +3,13 @@ package com.example.cs489groupproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.StringBuffer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            URL url = new URL("https://developers.strava.com/docs/reference/#api-Athletes-getLoggedInAthlete");
+            HttpURLConnection con =  (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+
+            Log.w("MA", "Here");
+            int status = con.getResponseCode();
+            Log.w("MA", ""+status);
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer content = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                content.append(inputLine);
+            }
+            System.out.println(content.toString());
+
+            // System.out.println(content);
+            // in.close();
+            // con.disconnect();
+        }
+        catch(Exception me) {
+            System.out.println("Catch");
+        }
     }
 }
