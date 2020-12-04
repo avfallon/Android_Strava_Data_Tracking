5189a458a74fefc0358a667ff5ee82d9892f22c9
avfallon <31700889+avfallon@users.noreply.github.com>
2020-12-04 18:20:15 -0500

    Volley Build

--- app/src/main/java/com/example/cs489groupproject/MainActivity.java
+++ app/src/main/java/com/example/cs489groupproject/MainActivity.java
@@ -11,6 +11,14 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.lang.StringBuffer;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -19,25 +27,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         try {
-            URL url = new URL("https://developers.strava.com/docs/reference/#api-Athletes-getLoggedInAthlete");
-            HttpURLConnection con =  (HttpURLConnection) url.openConnection();
-            con.setRequestMethod("GET");
+            RequestQueue ExampleRequestQueue = Volley.newRequestQueue(this);
+            String url = "https://developers.strava.com/docs/reference/#api-Athletes-getLoggedInAthlete";
+            StringRequest ExampleStringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    Log.w("MA", ""+response.substring(0,500));
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                }
+            });
 
-            Log.w("MA", "Here");
-            int status = con.getResponseCode();
-            Log.w("MA", ""+status);
+            ExampleRequestQueue.add(ExampleStringRequest);
 
-            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-            String inputLine;
-            StringBuffer content = new StringBuffer();
-            while ((inputLine = in.readLine()) != null) {
-                content.append(inputLine);
-            }
-            System.out.println(content.toString());
+//            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+//            String inputLine;
+//            StringBuffer content = new StringBuffer();
+//            while ((inputLine = in.readLine()) != null) {
+//                content.append(inputLine);
+//            }
+//            Log.w("MA", content.toString());
 
-            // System.out.println(content);
-            // in.close();
-            // con.disconnect();
         }
         catch(Exception me) {
             System.out.println("Catch");
