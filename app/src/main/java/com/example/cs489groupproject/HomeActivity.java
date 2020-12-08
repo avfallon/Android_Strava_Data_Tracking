9846b387a048fce790bbfe1188f32270a074ecde
Conor O'Brien <chobrien@loyola.edu>
2020-12-08 10:37:49 -0500

    Added Herve's speech recognition code for now

--- app/src/main/java/com/example/cs489groupproject/HomeActivity.java
+++ app/src/main/java/com/example/cs489groupproject/HomeActivity.java
@@ -2,13 +2,84 @@ package com.example.cs489groupproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity {
+
+    protected SpeechRecognizer speechRecognizer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void listen() {
+        if(this.speechRecognizer != null)
+            speechRecognizer.destroy();
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+
+        SpeechListener speechListener = new SpeechListener();
+        Intent speechIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        speechRecognizer.startListening(speechIntent);
+    }
+
+    public class SpeechListener implements RecognitionListener {
+
+        @Override
+        public void onReadyForSpeech(Bundle bundle) {
+            Log.w( "MA", "MA: inside onReadyForSpeech" );
+        }
+
+        @Override
+        public void onBeginningOfSpeech() {
+            Log.w( "MA", "MA: inside onBeginningOfSpeech" );
+        }
+
+        @Override
+        public void onRmsChanged(float v) {
+            Log.w( "MA", "MA: inside onRmsChanged" );
+        }
+
+        @Override
+        public void onBufferReceived(byte[] bytes) {
+            Log.w( "MA", "MA: inside onBufferReceived" );
+        }
+
+        @Override
+        public void onEndOfSpeech() {
+            Log.w( "MA", "MA: inside onEndOfSpeech" );
+        }
+
+        @Override
+        public void onError(int i) {
+            Log.w( "MA", "MA: inside onError, error is " + i  );
+            listen( );
+        }
+
+        @Override
+        public void onResults(Bundle bundle) {
+            ArrayList<String> words = bundle.getStringArrayList( SpeechRecognizer.RESULTS_RECOGNITION );
+            float [] scores = bundle.getFloatArray( SpeechRecognizer.CONFIDENCE_SCORES );
+
+            // idk what we are doing with the words
+        }
+
+        @Override
+        public void onPartialResults(Bundle bundle) {
+            Log.w( "MA", "MA: inside onPartialResults" );
+        }
+
+        @Override
+        public void onEvent(int i, Bundle bundle) {
+            Log.w( "MA", "MA: inside onEvent" );
+        }
     }
 }
