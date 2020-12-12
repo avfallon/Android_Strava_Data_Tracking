c72662ec3d935ceabe33cedf9a4fca9e521fc83f
mattkinloch <mjkinloch@loyola.edu>
2020-12-11 23:50:46 -0500

    ads work from returning from DA to HA

--- app/src/main/java/com/example/cs489groupproject/DataActivity.java
+++ app/src/main/java/com/example/cs489groupproject/DataActivity.java
@@ -2,20 +2,54 @@ package com.example.cs489groupproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
 
 public class DataActivity extends AppCompatActivity {
+
+    private static final String TOAST_TEXT = "Test ads are being shown. "
+            + "To show live ads, replace the ad unit ID in res/values/strings.xml with your own ad unit ID.";
+
+    private static final int START_LEVEL = 1;
+    private int mLevel;
+    private Button mNextLevelButton;
+    private InterstitialAd mInterstitialAd;
+    private TextView mLevelTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data);
         Log.w("MA", "DataActivity");
+
+        // Create the next level button, which tries to show an interstitial when clicked.
+        mNextLevelButton = ((Button) findViewById(R.id.data_button));
+        mNextLevelButton.setEnabled(false);
+        mNextLevelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showInterstitial();
+            }
+        });
+
+        // Create the text view to show the level number.
+        mLevelTextView = (TextView) findViewById(R.id.level);
+        mLevel = START_LEVEL;
+
+        // Create the InterstitialAd and set the adUnitId (defined in values/strings.xml).
+        mInterstitialAd = newInterstitialAd();
+        loadInterstitial();
 
         viewData();
     }
@@ -77,6 +111,63 @@ public class DataActivity extends AppCompatActivity {
     public void goBack( View v ) {
         finish();
         overridePendingTransition( R.anim.slide_from_left, 0 );
+    }
+
+    private InterstitialAd newInterstitialAd() {
+        Log.w("DA", "newInterstitialAd");
+        InterstitialAd interstitialAd = new InterstitialAd(this);
+        interstitialAd.setAdUnitId(getString(R.string.interstitial_ad_unit_id));
+        interstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                mNextLevelButton.setEnabled(true);
+            }
+
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                mNextLevelButton.setEnabled(true);
+            }
+
+            @Override
+            public void onAdClosed() {
+                // Proceed to the next level.
+                Log.w( "DA", "DA: inside onAdClosed" );
+                goToNextLevel();
+            }
+        });
+        return interstitialAd;
+    }
+
+    private void showInterstitial() {
+        // Show the ad if it"s ready. Otherwise toast and reload the ad.
+        Log.w("DA", "showInterstitial");
+
+        if (mInterstitialAd != null && mInterstitialAd.isLoaded()) {
+            mInterstitialAd.show();
+        } else {
+            Toast.makeText(this, "Ad did not load", Toast.LENGTH_SHORT).show();
+            goToNextLevel();
+        }
+    }
+
+    private void loadInterstitial() {
+        Log.w("DA", "loadInterstitial");
+        // Disable the next level button and load the ad.
+        mNextLevelButton.setEnabled(false);
+        AdRequest adRequest = new AdRequest.Builder()
+                .setRequestAgent("android_studio:ad_template").build();
+        mInterstitialAd.loadAd(adRequest);
+    }
+
+    private void goToNextLevel() {
+        Log.w("DA", "goToNextLevel");
+        // Show the next level and reload the ad to prepare for the level after.
+        // mLevelTextView.setText("Level " + (++mLevel));
+        mInterstitialAd = newInterstitialAd();
+        loadInterstitial();
+
+        Intent intent = new Intent( this, HomeActivity.class );
+        startActivity( intent );
     }
 
 }
